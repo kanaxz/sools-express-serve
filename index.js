4b1856b0cf1c744a1a3a453bd4a6b39f7e9a6a3a
kanaxz <kanaxstratz@gmail.com>
2024-08-24 19:50:21 +0200

    1.22.0

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
 const express = require('express')
 const http = require('http')
-const isbot = require('isbot')
+const https = require('https')
+const { isbot: getIsBot } = require('isbot')
 const { join } = require('path')
+const puppeteer = require('puppeteer')
+const PUPPETEER_FLAG = 'x-sools-express-puppeteer'
 
 module.exports = {
   name: 'express-serve',
   construct: ({ }, { serve: config }) => {
     const app = express()
-    const protocol = http
-    const server = protocol.createServer({}, app)
+    const protocol = config.mode === 'http' ? http : https
+    const server = protocol.createServer({
+      ...(config.options || {})
+    }, app)
 
     const renderPage = async (url) => {
-      const browser = await puppeteer.launch();
-      const page = await browser.newPage();
+      const browser = await puppeteer.launch({
+        ...(config.puppeteer || {})
+      })
+      const page = await browser.newPage()
+      await page.setExtraHTTPHeaders({
+        [PUPPETEER_FLAG]: '1'
+      });
 
+      console.log(url)
       await page.goto(url, { waitUntil: 'domcontentloaded' })
+
       await page.waitForFunction(() => {
-        return document.body.classList.contains('sools-app-loaded')
+        console.log(document.body.className)
+        return document.body.classList.contains('sools-app-ready')
       })
 
       await page.evaluate(() => {
-        const scripts = document.querySelectorAll('script')
-        scripts.forEach(script => script.remove())
+        const scripts = [...document.querySelectorAll('script')]
+        scripts
+          .filter((script) => script.getAttribute('keep-script') === null)
+          .forEach(script => script.remove())
       })
 
       const html = await page.content()
@@ -30,10 +45,16 @@ module.exports = {
       return html;
     }
 
-    app.use(express.static(config.dist))
+    app.use(express.static(config.dist, {
+      index: false,
+    }))
+
     app.use('/*', async (req, res) => {
-      if (isbot(req.headers['user-agent'])) {
-        const html = await renderPage(req.url)
+      const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`
+      console.log(fullUrl)
+      const isBot = /*true || /**/ getIsBot(req.headers['user-agent'])
+      if (isBot && req.headers[PUPPETEER_FLAG] !== '1') {
+        const html = await renderPage(fullUrl)
         res.send(html)
       } else {
         res.sendFile(join(config.dist, 'index.html'))
